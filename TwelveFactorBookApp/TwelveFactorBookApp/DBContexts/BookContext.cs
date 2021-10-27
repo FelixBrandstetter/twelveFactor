42ad1383ff4406cd6985aeaec9e8fed1ef3185f1
Felix Brandstetter <felix.brandstetter@fhwn.ac.at>
2021-10-27 20:15:53 +0200

    initial commit

---
+++ TwelveFactorBookApp/TwelveFactorBookApp/DBContexts/BookContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TwelveFactorBookApp.Models;
+
+namespace TwelveFactorBookApp.DBContexts
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
